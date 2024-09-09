4de3c3c9c66d8b223b05994602389c195c6f754e
이승구 <leesg107@naver.com>
2024-09-09 18:37:59 +0900

    test: 동시성 요청이 실패하는 케이스 테스트

--- api/src/test/java/com/ealrybird/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/ealrybird/api/service/ApplyServiceTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,5 +34,34 @@ class ApplyServiceTest {
         // then
         assertThat(count).isEqualTo(1);
 
+    }
+
+    @Test
+    @DisplayName("여러번 요청 - 동시성 실패")
+    void multi_apply_fail () throws InterruptedException {
+        // given
+        int treadCount = 1000;
+
+        ExecutorService service = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(treadCount);
+        // when
+
+        for (int i = 0; i < treadCount; i++) {
+             long userId = i;
+            service.submit(() -> {
+                try{
+                    applyService.apply(userId);
+                }finally {
+                    latch.countDown();
+                }
+            });
+
+        }
+
+        latch.await();
+
+        // then
+        long count = couponRepository.count();
+        assertThat(count).isNotEqualTo(treadCount);
     }
 }
