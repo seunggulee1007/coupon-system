8a18407ab10dc42b0b92b5b8aa806131fbc95ce2
이승구 <leesg107@naver.com>
2024-09-09 18:22:15 +0900

    feat: 쿠폰 발급 및 테스트

---
+++ api/src/main/java/com/ealrybird/api/repository/CouponRepository.java
@@ -0,0 +1,8 @@
+package com.ealrybird.api.repository;
+
+import com.ealrybird.api.domain.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+}
---
+++ api/src/test/java/com/ealrybird/api/service/ApplyServiceTest.java
@@ -0,0 +1,34 @@
+package com.ealrybird.api.service;
+
+import com.ealrybird.api.repository.CouponRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    @DisplayName("한번만 응모")
+    void  once_apply () {
+        // given
+        applyService.apply(1L);
+
+        // when
+        long count = couponRepository.count();
+
+        // then
+        assertThat(1).isEqualTo(count);
+
+    }
+}
